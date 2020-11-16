8e1f48c2422efff19f3fc6e1e5e4acc462696945
Levi Kennedy <lckennedy@gmail.com>
2020-11-16 09:33:39 -0600

    Initial commit

---
+++ midi-foot-switch.ino
@@ -0,0 +1,93 @@
+/*
+   This examples shows how to make a simple seven keys MIDI keyboard with volume control
+
+   Created: 4/10/2015
+   Author: Arturo Guadalupi <a.guadalupi@arduino.cc>
+
+   http://www.arduino.cc/en/Tutorial/MidiDevice
+*/
+
+#include <MIDIUSB.h>
+#include "PitchToNote.h"
+#define NUM_BUTTONS  5
+#define MIDI_CHANNEL 15
+
+const uint8_t button1 = 2;
+const uint8_t button2 = 3;
+const uint8_t button3 = 4;
+const uint8_t button4 = 5;
+const uint8_t button5 = 6;
+
+const uint8_t buttons[NUM_BUTTONS] = {button1, button2, button3, button4, button5};
+const byte notePitches[NUM_BUTTONS] = {C3, D3, E3, F3, G3};
+
+uint8_t notesTime[NUM_BUTTONS];
+uint8_t pressedButtons = 0x00;
+uint8_t previousButtons = 0x00;
+uint8_t intensity;
+
+void setup() {
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    pinMode(buttons[i], INPUT);
+    digitalWrite(buttons[i], HIGH);
+  }
+}
+
+
+void loop() {
+  readButtons();
+  playNotes();
+}
+
+void readButtons()
+{
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    if (digitalRead(buttons[i]) == LOW)
+    {
+      bitWrite(pressedButtons, i, 1);
+      delay(50);
+    }
+    else
+      bitWrite(pressedButtons, i, 0);
+  }
+}
+
+
+void playNotes()
+{
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    if (bitRead(pressedButtons, i) != bitRead(previousButtons, i))
+    {
+      if (bitRead(pressedButtons, i))
+      {
+        bitWrite(previousButtons, i , 1);
+        noteOn(MIDI_CHANNEL, notePitches[i], 64);
+        MidiUSB.flush();
+      }
+      else
+      {
+        bitWrite(previousButtons, i , 0);
+        noteOff(MIDI_CHANNEL, notePitches[i], 0);
+        MidiUSB.flush();
+      }
+    }
+  }
+}
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
